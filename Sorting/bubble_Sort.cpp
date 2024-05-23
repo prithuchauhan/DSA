d33a91a4e478b13b43ff3088c425e4c64af700ab
Prithu <132932509+prithuchauhan@users.noreply.github.com>
2024-05-23 08:58:59 +0530

    Update bubble_Sort.cpp

--- Sorting/bubble_Sort.cpp
+++ Sorting/bubble_Sort.cpp
@@ -1,1 +1,32 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+
+void bubbleSort(int arr[], int n) {
+    for (int i=1;i<n;i++){                             // n-1 iterations of swapping places (0 to n-1, 0 to n-2, 0 to n-3..... 0 to 1)
+        for(int j=0;j<n-i;j++){
+            if (arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);                 // swap places 
+            }
+        }
+    }   
+    }
+
+int main(){
+    int n;
+    cin>>n;
+
+    int array[n];
+    for(int i=0;i<n;i++) cin>>array[i];
+
+    bubbleSort(array,n);
+
+    for(int i=0;i<n;i++) cout<<array[i]<<" ";
+
+    return 0;
+}
