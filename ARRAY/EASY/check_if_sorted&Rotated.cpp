e3cadcc93380a35c7487608cc8c5c67694d06ec6
Prithu <132932509+prithuchauhan@users.noreply.github.com>
2024-05-24 15:52:29 +0530

    Update check_if_sorted&Rotated.cpp

--- ARRAY/EASY/check_if_sorted&Rotated.cpp
+++ ARRAY/EASY/check_if_sorted&Rotated.cpp
@@ -1,1 +1,30 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<math.h>
+
+using namespace std;
+using ll = long long;
+
+// QUESTION LINK : https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+
+bool check(vector<int>& nums) {
+
+        int count = 0;
+        for(int i=0;i<nums.size()-1;i++){
+            if (nums[i+1]<nums[i]){
+                count++;
+                if (count>1){
+                    return false;
+                }
+            }
+        }
+        if (nums[0]<nums[nums.size()-1]){
+            count++;
+        }
+
+        return (count>1) ? false : true;
+    }
