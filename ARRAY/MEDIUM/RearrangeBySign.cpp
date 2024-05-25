1c87b105d5a8c51431f550a6d12a84379f38b7f0
Prithu <132932509+prithuchauhan@users.noreply.github.com>
2024-05-25 19:55:07 +0530

    Add files via upload

---
+++ ARRAY/MEDIUM/RearrangeBySign.cpp
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+#include<bits/stdc++.h>
+#include<array>
+#include<vector>
+#include<string.h>
+#include<cmath>
+using namespace std;
+using lli = long long;
+
+vector<int> rearrangeArray(vector<int>& nums)
+    {
+        int plus = 0;
+        int minus = 0;
+        vector<int> ans;
+
+        int n = nums.size();
+
+        while(plus<n || minus<n){
+            
+            {
+                while(plus<n && nums[plus]<0){
+                    plus++;
+                }
+            }
+
+            {
+                while(minus<n && nums[minus]>=0){
+                    minus++;
+                }
+            }
+
+            if(plus<n)
+            {
+                ans.push_back(nums[plus]);
+                plus++;
+            }
+            if (minus<n)
+            {
+                ans.push_back(nums[minus]);
+                minus++;
+            }
+
+
+        }
+
+        
+
+        return ans;  
+    }
+
+int main(){
+
+    vector<int> nums = {-3,-3,-2,1,1,1};
+    vector <int> ans;
+
+    ans = rearrangeArray(nums);
+
+    for(int i=0;i<ans.size();i++) cout<<ans[i]<<" ";
+    
+
+
+    return 0;
+
+}
