5e8b8469d2a93dc6d3885ec6e24d66d329ee3fe9
Prithu <132932509+prithuchauhan@users.noreply.github.com>
2024-08-10 10:47:47 +0530

    Create CountSetBits.cpp

---
+++ Bit Manipulation/CountSetBits.cpp
@@ -0,0 +1,17 @@
+// QUESTION LINK : https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+
+        int ans = 0;
+        
+        for(int i=0;i<32;i++){
+            if ((n>>i)&1){
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
