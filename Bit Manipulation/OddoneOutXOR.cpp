d01eb2b085aebd863d5ad333c23fb31f2db0453e
Prithu <132932509+prithuchauhan@users.noreply.github.com>
2024-08-10 10:37:14 +0530

    Create OddoneOutXOR.cpp

---
+++ Bit Manipulation/OddoneOutXOR.cpp
@@ -0,0 +1,16 @@
+// QUESTION link : https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        
+        int ans = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            ans ^= nums[i];
+        }
+
+        return ans;
+    }
+};
