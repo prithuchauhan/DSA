565a7b982d9c364652bf003b07f9058315613a20
Prithu <132932509+prithuchauhan@users.noreply.github.com>
2024-08-21 18:15:55 +0530

    Create DecodeWays.cpp

---
+++ DP/DecodeWays.cpp
@@ -0,0 +1,36 @@
+// LINK : https://leetcode.com/problems/decode-ways/
+
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+        if (s[0] == '0'){
+            return 0;
+        }
+
+        int n = s.size();
+
+        vector<int> dp(n+1,0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            int one = stoi(s.substr(i-1,1));
+            if ((one)>=1 && one<=9){
+                dp[i] += dp[i-1];
+            }
+
+            int ten = stoi(s.substr(i-2,2));
+
+            if (ten >= 10 && ten<=26){
+                dp[i] += dp[i-2];           // similar to fibonacci;
+            }
+
+
+        }
+
+        return dp[n];
+    }
+};
